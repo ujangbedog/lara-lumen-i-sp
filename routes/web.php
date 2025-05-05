3ad595c288be9b7e620273828a97a1bd04226915
ujangbedog <domba88galing@gmail.com>
2025-05-05 13:43:39 +0700

    chore: use namespace folder

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,35 @@
 
 $router->get('/', 'HomeController@index');
 
-$router->get('/hello-lumen/{name}', function ($name) {
-    return "<h1>Lumen</h1><p>Hi <b>" . $name . "</b>, thank for using Lumen</p>";
+//test from folder
+$router->group(['namespace' => 'Coba'], function () use ($router) {
+    $router->get('/coba', 'CobaController@index');
 });
 
-$router->get('/scores', ['middleware' => 'login', function () {
-    return "<h1>Selamat!</h1><p>Nilai anda 100</p>";
-}]);
-
-$router->get('/users', 'UsersController@index');
-$router->get('/users/{id}', 'UsersController@show');
-
 //Latihan 1
-$router->get('/books', 'BookController@index');
-$router->get('/cities', 'CityController@index');
-$router->get('/profile/{id}', 'ProfileController@show');
-$router->get('/status', 'SystemController@status');
-$router->get('/secure-data', ['middleware' => 'apikey', 'uses' => 'SecureDataController@index']);
+$router->group(['namespace' => 'Latihan1'], function () use ($router) {
+    $router->get('/hello-lumen', function () {
+        return "<h1>Lumen</h1><p>Hi good developer, thank for using Lumen</p>";
+    });
+    $router->get('/hello-lumen/{name}', function ($name) {
+        return "<h1>Lumen</h1><p>Hi <b>" . $name . "</b>, thank for using Lumen</p>";
+    });
+
+    $router->get('/scores', ['middleware' => 'login', function () {
+        return "<h1>Selamat!</h1><p>Nilai anda 100</p>";
+    }]);
+
+    $router->get('/users', 'UsersController@index');
+    $router->get('/users/{id}', 'UsersController@show');
+
+    $router->get('/books', 'BookController@index');
+    $router->get('/cities', 'CityController@index');
+    $router->get('/profile/{id}', 'ProfileController@show');
+    $router->get('/status', 'SystemController@status');
+    $router->get('/secure-data', ['middleware' => 'apikey', 'uses' => 'SecureDataController@index']);
+});
 
 //Latihan 2
-$router->get('/posts', 'PostController@index');
+$router->group(['namespace' => 'Latihan2'], function () use ($router) {
+    $router->get('/posts', 'PostController@index');
+});
