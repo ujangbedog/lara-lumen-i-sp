8a7bccd25fa1db9fcb9d22b7650738b688e2afc0
ujangbedog <domba88galing@gmail.com>
2025-05-06 13:42:23 +0700

    chore: add docs endpoint to home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,21 +4,67 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        return "Lumen Controller";
-    }
-
     public function index()
     {
         return response()->json([
             'service_name' => 'PHP Service App',
             'status' => 'Running',
+            'endpoints' => [
+                'latihan1' => [
+                    '/latihan1/hello-lumen',
+                    '/latihan1/hello-lumen/{name}',
+                    '/latihan1/scores (with login middleware)',
+                    '/latihan1/users',
+                    '/latihan1/users/{id}',
+                    '/latihan1/books',
+                    '/latihan1/cities',
+                    '/latihan1/profile/{id}',
+                    '/latihan1/status',
+                    '/latihan1/secure-data (with apikey middleware)',
+                ],
+                'latihan2' => [
+                    '/latihan2/posts',
+                    '/latihan2/articles',
+                    '/latihan2/articles/{id}',
+                    '/latihan2/articles (POST)',
+                    '/latihan2/authors',
+                    '/latihan2/categories',
+                    '/latihan2/comments',
+                    '/latihan2/tags',
+                ],
+                'latihan3' => [
+                    '/latihan3/posts',
+                    '/latihan3/posts (POST)',
+                    '/latihan3/post/{id}',
+                    '/latihan3/post/{id} (PUT)',
+                    '/latihan3/post/{id} (DELETE)',
+                    '/latihan3/products',
+                    '/latihan3/products (POST)',
+                    '/latihan3/product/{id}',
+                    '/latihan3/product/{id} (PUT)',
+                    '/latihan3/product/{id} (DELETE)',
+                ],
+                'uts' => [
+                    '/uts/customers',
+                    '/uts/customers (POST)',
+                    '/uts/customer/{id}',
+                    '/uts/customer/{id} (PUT)',
+                    '/uts/customer/{id} (DELETE)',
+                    '/uts/orders',
+                    '/uts/orders (POST)',
+                    '/uts/order/{id}',
+                    '/uts/order/{id} (PUT)',
+                    '/uts/order/{id} (DELETE)',
+                    '/uts/employees',
+                    '/uts/employees (POST)',
+                    '/uts/employee/{id}',
+                    '/uts/employee/{id} (PUT)',
+                    '/uts/employee/{id} (DELETE)',
+                ],
+                'coba' => [
+                    '/coba',
+                ],
+            ],
         ]);
     }
 }
